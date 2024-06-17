0948d2933be25005d246559ac98e786eca3e97f6
Lukas Burger <lukas.burger@igramul.ch>
2024-06-17 21:47:51 +0200

    refactoring taupunkt.py

--- taupunkt.py
+++ taupunkt.py
@@ -1,23 +1,24 @@
 import math
 
-def taupunkt(t, r):
+
+def taupunkt(T, r):
     # Taupunkt Formel: https://www.wetterochs.de/wetter/feuchte.html
-    if t >= 0:
+    if T >= 0:
         a = 7.5
         b = 237.3
-    else: #  t < 0
+    else:  # t < 0
         a = 7.6
         b = 240.7
 
     # Sättigungsdampfdruck in hPa
-    sdd = 6.1078 * pow(10, (a*t)/(b+t))
+    SDD = 6.1078 * pow(10, (a * T) / (b + T))
 
     # Dampfdruck in hPa
-    dd = sdd * (r/100)
+    DD = (r/100) * SDD
 
     # v-Parameter
-    v = math.log10(dd/6.1078)
+    v = math.log10(DD/6.1078)
 
     # Taupunkttemperatur (°C)
-    tt = (b*v) / (a-v)
-    return tt
+    TD = b * v / (a-v)
+    return TD
