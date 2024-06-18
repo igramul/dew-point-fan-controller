0df54401c10d6f0c2503e1c4d07f29f812f4b9a7
Lukas Burger <lukas.burger@igramul.ch>
2024-06-18 20:18:24 +0200

    move wlan into a class

--- firmware/dew_point_fan_controller.py
+++ firmware/dew_point_fan_controller.py
@@ -2,31 +2,25 @@
 # January 2024, by Lukas Burger
 
 import machine
-import rp2
+
 import dht
 import time
-import ubinascii
-import network
-import ntptime
 import ujson
 import uasyncio as asyncio
+
 import sys
-
-
 sys.path.append('micropython_i2c_lcd')
 
 from pcf8574 import PCF8574
 from hd44780 import HD44780
 from lcd import LCD
 
-from networkstat import NetworkStat, STAT_NO_IP
 import measurementdata
 import dewpointfancontroller
 import webserver
+import wlan
 
 from version import version
-
-ntptime.host = '1.europe.pool.ntp.org'  # default time server
 
 led_wlan = machine.Pin(0, machine.Pin.OUT)
 led_fan_status = machine.Pin(18, machine.Pin.OUT, value=0)
@@ -48,77 +42,25 @@ touch_lcd_on = machine.Pin(12, machine.Pin.IN, machine.Pin.PULL_DOWN)
 timer_lcd_light = machine.Timer()
 
 
-def iluminate_lcd_background():
+def illuminate_lcd_background():
     print('LCD backlight on for 60s.')
     lcd.backlight_on()
     timer_lcd_light.init(mode=machine.Timer.ONE_SHOT, period=60000, callback=lambda t: lcd.backlight_off())
 
 
-touch_lcd_on.irq(lambda irq: iluminate_lcd_background(), machine.Pin.IRQ_RISING)
+touch_lcd_on.irq(lambda irq: illuminate_lcd_background(), machine.Pin.IRQ_RISING)
 
 lcd.write_line('Dew Point Fan Contr.', 0)
 lcd.write_line('--------------------', 1)
 lcd.write_line(f'Version {version}', 2)
 lcd.write_line('lburger@igramul.ch', 3)
-iluminate_lcd_background()
+illuminate_lcd_background()
 
 with open('config.json') as fp:
     config = ujson.loads(fp.read())
 
 with open('secrets.json') as fp:
     secrets = ujson.loads(fp.read())
-
-
-def wlan_connect():
-    global secrets
-    if not wlan.isconnected():
-        led_wlan.off()
-        wlan.active(False)
-        print('WLAN connecting')
-        wlan.active(True)
-        mac = ubinascii.hexlify(network.WLAN().config('mac'), ':').decode()
-        print(f'--> MAC address: {mac}')
-        if mac == '00:00:00:00:00:00':
-            raise RuntimeError('Invalid Mac Address')
-        wlan.config(pm = 0xa11140) # disable WiFi power-save mode for a web server
-
-        wlans = wlan.scan()
-        search_wlan = True
-        for (ssid, bssid, channel, RSSI, security, hidden) in wlans:
-            print(f'Found WLAN with ssid: {ssid}')
-            for (my_ssid, my_passwd) in [(_.get('ssid'), _.get('password')) for _ in secrets.get('wlans')]:
-                if ssid.decode('utf8') == my_ssid:
-                    search_wlan = False
-                    print(f'  Match! WLAN credentials found.')
-                    lcd.write_line(f'WLAN: {my_ssid}', 1)
-                    wlan.connect(my_ssid, my_passwd)
-                    break
-            if not search_wlan:
-                break
-
-        for _ in range(10):
-            wlan_status = wlan.status()
-            if wlan_status not in [network.STAT_CONNECTING, STAT_NO_IP]:
-                break
-            print('--> WLAN status:', NetworkStat[wlan_status])
-            led_wlan.toggle()
-            time.sleep(0.25)
-            led_wlan.toggle()
-            time.sleep(0.25)
-
-    if wlan.isconnected():
-        print('WLAN connection ok')
-        led_wlan.on()
-        print('WLAN status:', NetworkStat[wlan.status()])
-        net_config = wlan.ifconfig()
-        print('  - IPv4 addresse', net_config[0], '/', net_config[1])
-        print('  - standard gateway:', net_config[2])
-        print('  - DNS server:', net_config[3])
-        ntptime.settime()
-    else:
-        print('WLAN connection failed')
-        print('WLAN status:', NetworkStat[wlan.status()])
-        raise RuntimeError('No WLAN connection')
 
 
 def get_time_string(t):
@@ -128,7 +70,7 @@ def get_time_string(t):
 def tick(timer):
     t = time.localtime()
     time_utc = get_time_string(t)
-    if wlan.isconnected():
+    if wlan.is_connected:
         lcd.write_line(time_utc, 0)
     else:
         lcd.write_line('Dew Point Fan Contr.', 0)
@@ -151,12 +93,14 @@ dew_point_fan_controller = dewpointfancontroller.DewPointFanController(sensor_in
 
 web_server = webserver.WebServer(dew_point_fan_controller=dew_point_fan_controller)
 
+wlan = wlan.MicroPythonWlan(config=config, secrets=secrets, led=led_wlan, lcd=lcd)
+
 
 async def main():
     print('Initial dew point measurement')
     measurement(get_time_string(time.localtime()))
 
-    if wlan.isconnected():
+    if wlan.is_connected:
         print('Setting up Webserver')
         asyncio.create_task(asyncio.start_server(web_server.serve_client, '0.0.0.0', 80))
 
@@ -174,24 +118,6 @@ async def main():
 
 timer_measurement = machine.Timer()
 
-# set Wi-Fi Country
-rp2.country(config.get('Wi-Fi Country'))
-network.country(config.get('Wi-Fi Country'))
-
-# set hostname
-network.hostname(config.get('Hostname'))
-
-wlan = network.WLAN(network.STA_IF)
-for i in range(4):
-    try:
-        wlan_connect()
-    except RuntimeError as e:
-        lcd.write_line('Network Error %i' % i, 0)
-        lcd.write_line('%s' % e, 1)
-        print('WLAN status:', NetworkStat[wlan.status()])
-        print('Exception: %s' % e)
-    if wlan.isconnected():
-        lcd.write_line(f'Hostname: {config.get("Hostname")}', 2)
-        break
+wlan.start()
 
 asyncio.run(main())
---
+++ firmware/wlan.py
@@ -0,0 +1,95 @@
+import time
+import rp2
+import network
+import ntptime
+import ubinascii
+
+from networkstat import NetworkStat, STAT_NO_IP
+
+
+class MicroPythonWlan(object):
+
+    def __init__(self, config, secrets, led, lcd):
+        self._config = config
+        self._secrets = secrets
+        self._led = led
+        self._lcd = lcd
+
+        # set Wi-Fi Country
+        rp2.country(self._config.get('Wi-Fi Country'))
+        network.country(self._config.get('Wi-Fi Country'))
+
+        # set hostname
+        network.hostname(self._config.get('Hostname'))
+
+        # default time server
+        ntptime.host = '1.europe.pool.ntp.org'
+
+        self._wlan = network.WLAN(network.STA_IF)
+
+    def start(self):
+        for i in range(4):
+            try:
+                self._wlan_connect()
+            except RuntimeError as e:
+                self._lcd.write_line('Network Error %i' % i, 0)
+                self._lcd.write_line('%s' % e, 1)
+                print('WLAN status:', NetworkStat[self._wlan.status()])
+                print('Exception: %s' % e)
+            if self._wlan.isconnected():
+                self._lcd.write_line(f'Hostname: {self._config.get("Hostname")}', 2)
+                break
+
+    @property
+    def is_connected(self):
+        return self._wlan.isconnected()
+
+    def _wlan_connect(self):
+        if not self._wlan.isconnected():
+            self._led.off()
+            self._wlan.active(False)
+            print('WLAN connecting')
+            self._wlan.active(True)
+            mac = ubinascii.hexlify(network.WLAN().config('mac'), ':').decode()
+            print(f'--> MAC address: {mac}')
+            if mac == '00:00:00:00:00:00':
+                raise RuntimeError('Invalid Mac Address')
+            self._wlan.config(pm=0xa11140)  # disable WiFi power-save mode for a web server
+
+            wlans = self._wlan.scan()
+            search_wlan = True
+            for (ssid, bssid, channel, RSSI, security, hidden) in wlans:
+                print(f'Found WLAN with ssid: {ssid}')
+                for (my_ssid, my_passwd) in [(_.get('ssid'), _.get('password')) for _ in self._secrets.get('wlans')]:
+                    if ssid.decode('utf8') == my_ssid:
+                        search_wlan = False
+                        print(f'  Match! WLAN credentials found.')
+                        self._lcd.write_line(f'WLAN: {my_ssid}', 1)
+                        self._wlan.connect(my_ssid, my_passwd)
+                        break
+                if not search_wlan:
+                    break
+
+            for _ in range(10):
+                wlan_status = self._wlan.status()
+                if wlan_status not in [network.STAT_CONNECTING, STAT_NO_IP]:
+                    break
+                print('--> WLAN status:', NetworkStat[wlan_status])
+                self._led.toggle()
+                time.sleep(0.25)
+                self._led.toggle()
+                time.sleep(0.25)
+
+        if self._wlan.isconnected():
+            print('WLAN connection ok')
+            self._led.on()
+            print('WLAN status:', NetworkStat[self._wlan.status()])
+            net_config = self._wlan.ifconfig()
+            print('  - IPv4 addresse', net_config[0], '/', net_config[1])
+            print('  - standard gateway:', net_config[2])
+            print('  - DNS server:', net_config[3])
+            ntptime.settime()
+        else:
+            print('WLAN connection failed')
+            print('WLAN status:', NetworkStat[self._wlan.status()])
+            raise RuntimeError('No WLAN connection')
