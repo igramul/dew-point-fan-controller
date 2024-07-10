4eee84a7bbb5482a6b2ca78a21a59c819ead8f88
Lukas Burger <lukas.burger@igramul.ch>
2024-07-10 21:19:58 +0200

    refactoring

--- firmware/dew_point_fan_controller.py
+++ firmware/dew_point_fan_controller.py
@@ -9,8 +9,8 @@ import uasyncio as asyncio
 from measurementdata import MeasurementData
 from dewpointfancontroller import DewPointFanController
 from display import Display
-import webserver
-import wlan
+from webserver import WebServer
+from wlan import MicroPythonWlan
 
 from version import version
 
@@ -30,19 +30,17 @@ fan_status = machine.Pin(13, machine.Pin.IN)
 sensor_indoor = dht.DHT22(machine.Pin(6))
 sensor_outdoor = dht.DHT22(machine.Pin(7))
 
-i2c = machine.I2C(1, sda=machine.Pin(2), scl=machine.Pin(3), freq=400000)
-display = Display(i2c=i2c)
-
-timer_measurement = machine.Timer()
+display = Display(i2c=machine.I2C(1, sda=machine.Pin(2), scl=machine.Pin(3), freq=400000))
 
 measurement_data = MeasurementData()
 dew_point_fan_controller = DewPointFanController(sensor_indoor, sensor_outdoor, version, measurement_data)
-web_server = webserver.WebServer(dew_point_fan_controller=dew_point_fan_controller)
-wlan = wlan.MicroPythonWlan(config=config, secrets=secrets, led=led_wlan, display=display)
+web_server = WebServer(dew_point_fan_controller=dew_point_fan_controller)
+wlan = MicroPythonWlan(config=config, secrets=secrets, led=led_wlan, display=display)
 
 
 def get_time_string(t):
-    return '%02i.%02i.%02i %02i:%02i:%02i' % (t[2], t[1], t[0], t[3], t[4], t[5])
+    (year, month, mday, hour, minute, second, weekday, yearday) = t
+    return '%02i.%02i.%02i %02i:%02i:%02i' % (mday, month, year, hour, minute, second)
 
 
 def tick(timer):
@@ -74,7 +72,7 @@ async def main():
         asyncio.create_task(asyncio.start_server(web_server.serve_client, '0.0.0.0', 80))
 
     print('Running Dew Point Fan Controller')
-    timer_measurement.init(period=1000, mode=machine.Timer.PERIODIC, callback=tick)
+    machine.Timer().init(period=1000, mode=machine.Timer.PERIODIC, callback=tick)
 
     wdt = machine.WDT(timeout=8388)  # enable watchdog with a timeout of 8.3s
 
