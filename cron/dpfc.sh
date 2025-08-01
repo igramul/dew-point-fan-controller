d93eafde9626397ee81c105f17d46ef99d4b4ea3
u237168 <lukas.burger@sbb.ch>
2025-08-01 12:58:30 +0200

    refactoring of dpfc script

--- cron/dpfc.sh
+++ cron/dpfc.sh
@@ -16,59 +16,77 @@ temp_indoor_min=10.0
 # minimum outdoor temperature at which the ventilation is activated
 temp_outdoor_min=-10.0
 
-PROMETHEUS_QUERY_URL="http://nisset:8080/api/v1/query?query="
+PROMETHEUS_QUERY_URL="http://nisset:32090/api/v1/query?query="
 SWITCH_URL="http://192.168.10.44/relay"
 
-indoor_temp=$(curl -s "${PROMETHEUS_QUERY_URL}avg_over_time(indoor_temp\[5m\])" | jq --raw-output .data.result[0].value[1])
+# Function to Retrieve a Metric
+get_metric() {
+  query="$1"
+  curl -s "${PROMETHEUS_QUERY_URL}${query}" | jq --raw-output .data.result[0].value[1]
+}
 
+# Retrieve Metrics
+indoor_temp=$(get_metric "avg_over_time(indoor_temp\[5m\])")
+if [ $? -ne 0 ] || [ -z "$indoor_temp" ]; then
+  echo "Error retrieving indoor temperature. Script will be aborted."
+  exit 1
+fi
 if [ "${indoor_temp}" == "null" ]; then
   echo "can not calculate new fan state because measurement value is null"
   exit
 fi
 
-outdoor_temp=$(curl -s "${PROMETHEUS_QUERY_URL}avg_over_time(outdoor_temp\[5m\])" | jq --raw-output .data.result[0].value[1])
-indoor_dew_point=$(curl -s "${PROMETHEUS_QUERY_URL}avg_over_time(indoor_dew_point\[5m\])" | jq --raw-output .data.result[0].value[1])
-outdoor_dew_point=$(curl -s "${PROMETHEUS_QUERY_URL}avg_over_time(outdoor_dew_point\[5m\])" | jq --raw-output .data.result[0].value[1])
+outdoor_temp=$(get_metric "avg_over_time(outdoor_temp\[5m\])")
+indoor_dew_point=$(get_metric "avg_over_time(indoor_dew_point\[5m\])")
+outdoor_dew_point=$(get_metric "avg_over_time(outdoor_dew_point\[5m\])")
 
-echo "indoor_temp=${indoor_temp} indoor_dew_point=${indoor_dew_point}"
-echo "outdoor_temp=${outdoor_temp} outdoor_dew_point=${outdoor_dew_point}"
+echo "indoor_temp=${indoor_temp} | indoor_dew_point=${indoor_dew_point}"
+echo "outdoor_temp=${outdoor_temp} | outdoor_dew_point=${outdoor_dew_point}"
 
+# Difference of Dew Points
 dew_point_delta=$(bc -l <<< "${indoor_dew_point}-${outdoor_dew_point}")
 echo "dew_point_delta=${dew_point_delta}"
 
-fan_control_state=$(curl -s ${SWITCH_URL} | jq --raw-output .on)
-echo current fan state="${fan_control_state}"
+# Retrieve Current Fan Status
+fan_state=$(curl -s ${SWITCH_URL} | jq --raw-output .on)
+if [ $? -ne 0 ] || [ -z "$fan_state" ]; then
+  echo "Error retrieving fan status. Script will be aborted."
+  exit 1
+fi
+echo fan_state="${fan_state}"
 
-fan_control="${fan_control_state}"
+# Determine Desired State (true = Fan On, false = Fan Off)
+desired="false"
 
-if (test $(bc -l <<< "${dew_point_delta} > (${switch_min} + ${hysteresis})") -eq 1); then
-  echo "set fan_control=true (dew point threshold passed)"
-  fan_control=true
+if [ $(bc -l <<< "${dew_point_delta} > (${switch_min} + ${hysteresis})") -eq 1 ]; then
+  echo "Dew point difference exceeds threshold: Fan On"
+  desired="true"
 fi
 
-if (test $(bc -l <<< "${dew_point_delta} < ${switch_min}") -eq 1); then
-  echo "set fan_control=false (dew point threshold undershoot)"
-  fan_control=false
+if [ $(bc -l <<< "${dew_point_delta} < ${switch_min}") -eq 1 ]; then
+  echo "Dew point difference falls below threshold: Fan Off"
+  desired="false"
 fi
 
-if (test $(bc -l <<< "${indoor_temp} < ${temp_indoor_min}") -eq 1); then
-  echo "set fan_control=false (indoor temp is too low for ventilation)"
-  fan_control=false
+if [ $(bc -l <<< "${indoor_temp} < ${temp_indoor_min}") -eq 1 ]; then
+  echo "Indoor temperature too low: Fan Off"
+  desired="false"
 fi
 
-if (test $(bc -l <<< "${outdoor_temp} < ${temp_outdoor_min}") -eq 1); then
-  echo "set fan_control=false (outdoor temp is too low for ventilation)"
-  fan_control=false
+if [ $(bc -l <<< "${outdoor_temp} < ${temp_outdoor_min}") -eq 1 ]; then
+  echo "Outdoor temperature too low: Fan Off"
+  desired="false"
 fi
 
-if [ "${fan_control_state}" == "${fan_control}" ]; then
-  echo Fan state unchanged
+# Make changes only if the state changes
+if [ "${fan_state}" = "${desired}" ]; then
+  echo "Fan status unchanged."
 else
-  if (${fan_control} eq "true"); then
+  if [ "${desired}" = "true" ]; then
     echo "switching fan on"
-    curl ${SWITCH_URL}?state=1
+    curl "${SWITCH_URL}?state=1"
   else
     echo "switching fan off"
-    curl ${SWITCH_URL}?state=0
+    curl "${SWITCH_URL}?state=0"
   fi
 fi
